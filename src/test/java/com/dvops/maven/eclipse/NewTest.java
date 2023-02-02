168150d949cd9a0826429dd0959e771894844899
hhamm <hhamm@172.30.70.197>
2023-02-02 21:36:47 +0800

    test

--- src/test/java/com/dvops/maven/eclipse/NewTest.java
+++ src/test/java/com/dvops/maven/eclipse/NewTest.java
@@ -29,7 +29,7 @@ public class NewTest {
 	  webDriver.navigate().to("http://localhost:8090/devops-web-project-deployment/");
 	  
 	  //Assert the title to check that we are indeed in the correct website
-	  Assert.assertEquals(webDriver.getTitle(), "To do list");
+	  Assert.assertEquals(webDriver.getTitle(), "To do list!");
 	  
 System.out.println("title: "+webDriver.getTitle());
 	  
