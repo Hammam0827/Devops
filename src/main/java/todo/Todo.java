f152a2e19a8a7e056dd547a3d32418d47b19fc48
hhamm <hhamm@172.30.70.197>
2023-02-02 16:49:31 +0800

    test

--- src/main/java/todo/Todo.java
+++ src/main/java/todo/Todo.java
@@ -1,42 +1,42 @@
-//package todo;
-//
-//public class Todo {
-//	protected String todo;
-//	
-//	public Todo(String todo, String description, String date, String time) {
-//		super();
-//		this.todo = todo;
-//		this.description = description;
-//		this.date = date;
-//		this.time = time;
-//	}
-//	
-//	public String getTodo() {
-//		return todo;
-//	}
-//	public void setTodo(String todo) {
-//		this.todo = todo;
-//	}
-//	public String getDescription() {
-//		return description;
-//	}
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-//	public String getDate() {
-//		return date;
-//	}
-//	public void setDate(String date) {
-//		this.date = date;
-//	}
-//	public String getTime() {
-//		return time;
-//	}
-//	public void setTime(String time) {
-//		this.time = time;
-//	}
-//	protected String description;
-//	protected String date;
-//	protected String time;
-//
-//}
+package todo;
+
+public class Todo {
+	protected String todo;
+	
+	public Todo(String todo, String description, String date, String time) {
+		super();
+		this.todo = todo;
+		this.description = description;
+		this.date = date;
+		this.time = time;
+	}
+	
+	public String getTodo() {
+		return todo;
+	}
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	protected String description;
+	protected String date;
+	protected String time;
+
+}
